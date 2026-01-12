54234f5e812158c23dae382e7de65baec82d4bbb
Zarex <118372151+zarex9@users.noreply.github.com>
2026-01-12 14:47:30 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,7 @@ module.exports = {
                                       chainId: 44787
                 },
                     celo: {
-                              url: process.env.CELO_RPC || 'https://forno.celo.org',
+                              url: process.env.CELO_RPC || 'https://forno.cel.org',
                                     accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
                                           chainId: 42220
                     },

cbf1686873c86e1f779b4b8fe15a355f8b666ed6
Zarex <118372151+zarex9@users.noreply.github.com>
2026-01-12 16:51:56 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 const hre = require('hardhat');
 
-function parseUnitsSafe(value, unit = 'ethe') {
+function parseUnitsSafe(value, unit = 'ether') {
   // Support both ethers v6 (hre.ethers.parseUnits) and v5 (hre.ethers.utils.parseUnits)
   if (!value) value = '0.01';
   try {
